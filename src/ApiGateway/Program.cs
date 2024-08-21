093e429ca3eeeafe1f29613e7f9b6c41e6bd9fcd
aleksandrkarkac <oleksandrk@textunited.com>
2024-08-21 20:54:31 +0200

    Remove unused Docker workflows and update ApiGateway configuration

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

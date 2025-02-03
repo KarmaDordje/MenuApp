f28e59a9063222890225b9803715c2007222e311
aleksandrkarkac <oleksandrk@textunited.com>
2025-02-03 22:14:32 +0100

    adden files to Account ms

---
+++ src/Account/Account.API/Program.cs
@@ -0,0 +1,35 @@
+// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using Account.API;
+using Account.Application;
+using Account.Infrastructure;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services
+    .AddGlobalErrorHandling()
+    .AddPersistence()
+    .AddApplication();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
